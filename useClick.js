86cc7388921bf6289eab59fac77938afbb000692
Yeongmin_Chae <codudals98@naver.com>
2022-06-22 23:33:56 +0900

    fifth hook : useClick

---
+++ useClick.js
@@ -0,0 +1,28 @@
+import { useState, React, useEffect, useRef } from "react";
+import "./styles.css";
+
+const useClick = (onClick) => {
+  if (typeof onclick != "function") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => element.current.removeEventListener("click", onClick);
+  }, []);
+  return element;
+};
+
+const App = () => {
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+};
+
+export default App;
