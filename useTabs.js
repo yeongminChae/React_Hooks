35685eec7d722534b0830d6df0462f1c60ca4f90
Yeongmin_Chae <codudals98@naver.com>
2022-06-22 17:34:28 +0900

    Third hook : useTabs

---
+++ useTabs.js
@@ -0,0 +1,38 @@
+import { useState, React } from "react";
+import "./styles.css";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section2",
+  },
+];
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
+
+const App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
+  return (
+    <div className="App">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </div>
+  );
+};
+
+export default App;
