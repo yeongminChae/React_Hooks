be1f38eceba5f29b23a2636a13592fd4a9073703
Yeongmin_Chae <codudals98@naver.com>
2022-06-22 17:15:55 +0900

    first hook : useState

---
+++ useState.js
@@ -0,0 +1,20 @@
+import { Component, useState, React } from "react";
+import "./styles.css";
+
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
+    </div>
+  );
+};
+
+const useInput = () => {};
+
+export default App;
